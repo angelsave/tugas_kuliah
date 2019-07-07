8a5314167c8d5fcd2683264ed1188e2183f91d28
angelsave <didikariyanto301@gmail.com>
2019-07-08 01:34:15 +0700

    perulangan ganjil-genap

---
+++ perulangan/for.php
@@ -0,0 +1,17 @@
+<?php
+
+//Menampilkan nilai ganjil 1 3 5 7 9 11 13 15 17 19 21
+$a=1;
+for($a; $a<=21; $a+=2){
+	echo "Nilai $a Adalah Ganjil <br>";
+}
+
+echo "<hr color='blue'>";
+
+//Menampilkan nilai genap  2 4 6 8 10 12 14 16 18 20 
+$b=2;
+for($b; $b<=20; $b+=2){
+	echo "Nilai $b Adalah Genap <br>";
+}
+
+?>
