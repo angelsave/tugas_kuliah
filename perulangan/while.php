c6740dea0775a07935c45bd9486ef435585d1ff9
angelsave <didikariyanto301@gmail.com>
2019-07-08 01:36:44 +0700

    perulangan ganjil-genap

---
+++ perulangan/while.php
@@ -0,0 +1,15 @@
+<?php
+//Menampilkan nilai ganjil 1 3 5 7 9 11 13 15 17 19 21
+$a=1;
+while($a<=21){
+	echo "Nilai $a adalah ganjil<br>";
+	$a+=2;
+}
+echo "<br>";
+//Menampilkan nilai genap 2 4 6 8 10 12 14 16 18 20
+$b=2;
+while($b<=20){
+	echo "Nilai $b adalah genap<br>";
+	$b+=2;
+}
+?>
